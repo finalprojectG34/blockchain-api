7bb91888082a94aff275f3f85dab508ddb8757a0
nattigy <nathnaelakale@gmail.com>
2022-03-21 22:57:03 +0300

    test deployment on ganache cli

--- truffle-config.js
+++ truffle-config.js
@@ -6,18 +6,10 @@ const API_KEY = process.env.INFURA_API_KEY
 module.exports = {
 
   networks: {
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
-    rinkeby: {
-      provider: () => new HDWalletProvider(MNEMONIC, `https://rinkeby.infura.io/v3/${API_KEY}`),
-      network_id: 4,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
     },
   },
 
